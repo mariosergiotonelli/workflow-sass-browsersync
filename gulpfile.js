8710c5dc8bdef1be2db69d25560ec6742615d700
Mario Tonelli <mario.tonelli@driven.cx>
2020-07-20 21:42:59 -0300

    Atualizando workflow

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,43 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
-
+var uglify = require('gulp-uglify');
 
 //default gulp
-gulp.task('default', ['sass', 'watch', 'browser-sync']);
+gulp.task('default', ['sass', 'scripts', 'watch', 'browser-sync']);
 
 //Sass
 gulp.task('sass', function () {
-	return gulp.src('assets/src/sass/**/*.scss')
-	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-	.pipe(concat('style.css'))
-    .pipe(gulp.dest('assets/css'))    
-    .pipe(browserSync.stream());	
+  return gulp.src('src/sass/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(concat('projeto.min.css'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.reload({stream:true}));
+  });
+
+// JS
+gulp.task('scripts', function() {
+  return gulp.src("src/js/**/*.js")
+      .pipe(concat("projeto.min.js"))
+      .pipe(uglify())
+      .pipe(gulp.dest("dist/js"))
+      .pipe(browserSync.reload({stream:true}));
 });
 
 //browserSync
 gulp.task('browser-sync', ['sass'], function() {
 
-    browserSync.init({
-        server: "."
-    });
+  browserSync.init({
+    server: "."
+  });
 
-    gulp.watch("assets/src/sass/**/*.scss", ['sass']);
-    gulp.watch("./*.html").on('change', browserSync.reload);
+  gulp.watch("src/sass/**/*.scss", ['sass']);
+  gulp.watch("src/js/**/*.js", ['scripts']);
+  gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
 //Watch
 gulp.task('watch', function () {
-    gulp.watch('assets/src/sass/**/*.scss',['sass']);
+  gulp.watch('src/sass/**/*.scss',['sass']);
+  gulp.watch('src/js/**/*.js',['scripts']);
 });
